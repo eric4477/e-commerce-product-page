7af9a6b1b1949b1f3f8cf453e22d8a2620ea5504
Eric <ericgeorge4477@gmail.com>
2024-06-28 02:05:40 +0300

    remove fade

--- src/components/ProductImages/ProductImages.jsx
+++ src/components/ProductImages/ProductImages.jsx
@@ -5,21 +5,16 @@ import { useEffect, useState } from "react";
 function ProductImages() {
   const [activeIndex, setActiveIndex] = useState(0);
   const [activeImg, setActiveImg] = useState(productImgs[0]);
-  const [fade, setFade] = useState(false);
 
   const handleThumbnailClick = (index) => {
-    setFade(true);
-    setTimeout(() => {
-      setActiveIndex(index);
-      setFade(false);
-    }, 100);
+    setActiveIndex(index);
   };
   useEffect(() => {
     setActiveImg(productImgs[activeIndex]);
   }, [activeIndex]);
   return (
     <div className="product-images">
-      <div className={`product-image ${fade ? "fade" : ""}`}>
+      <div className="product-image">
         <img src={activeImg} alt={`Product Image ${activeIndex + 1}`} />
       </div>
       <div className="thumbnail-images">
