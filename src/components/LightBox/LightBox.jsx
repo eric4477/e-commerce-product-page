fb32a50d64a04ade785bd3e2f9d375f6f10f4f5c
Eric <ericgeorge4477@gmail.com>
2024-06-28 22:07:53 +0300

    complete LightBox component

---
+++ src/components/LightBox/LightBox.jsx
@@ -0,0 +1,97 @@
+import "./LightBox.css";
+import { thumbnails, productImgs } from "../../constants";
+import { IoCloseSharp } from "react-icons/io5";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+import { useState, useEffect, useRef } from "react";
+
+function LightBox({ activeIndex, openLightBox, setOpenLightBox }) {
+  let [currIndex, setCurrIndex] = useState(activeIndex);
+
+  const handleThumbnailClick = (index) => {
+    setCurrIndex(index);
+  };
+
+  const handleLeftArrowClick = () => {
+    setCurrIndex((prevIndex) => {
+      if (prevIndex === 0) {
+        return productImgs.length - 1;
+      }
+      console.log(currIndex);
+      return prevIndex - 1;
+    });
+  };
+
+  const handleRightArrowClick = () => {
+    setCurrIndex((prevIndex) => {
+      if (prevIndex >= productImgs.length - 1) {
+        return 0;
+      }
+      return prevIndex + 1;
+    });
+  };
+
+  const lightBoxRef = useRef(null);
+  // Effect to add click outside listener when lightbox is open
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (lightBoxRef.current && !lightBoxRef.current.contains(e.target)) {
+        setOpenLightBox(false);
+      }
+    };
+
+    if (openLightBox) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [openLightBox]);
+
+  return (
+    <div className="lightbox-wrapper">
+      <div ref={lightBoxRef} className="lightbox">
+        <div className="close-btn">
+          <IoCloseSharp onClick={() => setOpenLightBox(false)} />
+        </div>
+        <div className="lightbox-product-images">
+          <img
+            src={productImgs[currIndex]}
+            alt={`Product Image ${currIndex + 1}`}
+          />
+          <FaChevronLeft
+            className="left-arrow"
+            onClick={handleLeftArrowClick}
+          />
+          <FaChevronRight
+            className="right-arrow"
+            onClick={handleRightArrowClick}
+          />
+        </div>
+        <div className="lightbox-thumbnail-images">
+          {thumbnails.map((img, index) => (
+            <div
+              className={`lightbox-thumbnail-image ${
+                index === currIndex ? "active" : ""
+              }`}
+              style={{
+                outline:
+                  index === currIndex
+                    ? "2px solid var(--orange)"
+                    : "2px solid transparent",
+              }}
+              key={index}
+              onClick={() => handleThumbnailClick(index)}
+            >
+              <img src={img} alt={`Thumbnail Image ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LightBox;
--- src/components/ProductImages/ProductImages.jsx
+++ src/components/ProductImages/ProductImages.jsx
@@ -1,44 +1,59 @@
 import "./ProductImages.css";
 import { productImgs, thumbnails } from "./../../constants";
 import { useEffect, useState } from "react";
+import LightBox from "../LightBox/LightBox";
 
 function ProductImages() {
   const [activeIndex, setActiveIndex] = useState(0);
   const [activeImg, setActiveImg] = useState(productImgs[0]);
+  const [openLightBox, setOpenLightBox] = useState(false);
 
   const handleThumbnailClick = (index) => {
     setActiveIndex(index);
+  };
+  const handleImageClick = () => {
+    setOpenLightBox(!openLightBox);
   };
   useEffect(() => {
     setActiveImg(productImgs[activeIndex]);
   }, [activeIndex]);
   return (
-    <div className="product-images">
-      <div className="product-image">
-        <img src={activeImg} alt={`Product Image ${activeIndex + 1}`} />
+    <>
+      {openLightBox && (
+        <LightBox
+          activeIndex={activeIndex}
+          openLightBox={openLightBox}
+          setOpenLightBox={setOpenLightBox}
+        />
+      )}
+
+      <div className="product-images">
+        <div onClick={handleImageClick} className="product-image">
+          <img src={activeImg} alt={`Product Image ${activeIndex + 1}`} />
+        </div>
+        <div className="thumbnail-images">
+          {thumbnails.map((thumbnail, index) => (
+            <div
+              className="thumbnail-image"
+              style={{
+                border:
+                  index === activeIndex
+                    ? "2px solid var(--orange)"
+                    : "2px solid transparent",
+              }}
+              key={index}
+              onClick={() => handleThumbnailClick(index)}
+            >
+              <img
+                className={`${index === activeIndex ? "active-thumb" : ""}`}
+                src={thumbnail}
+                alt={`Thumbnail Image ${index + 1}`}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="thumbnail-images">
-        {thumbnails.map((thumbnail, index) => (
-          <div
-            className="thumbnail-image"
-            style={{
-              border:
-                index === activeIndex
-                  ? "2px solid var(--orange)"
-                  : "2px solid transparent",
-            }}
-            key={index}
-            onClick={() => handleThumbnailClick(index)}
-          >
-            <img
-              className={`${index === activeIndex ? "active-thumb" : ""}`}
-              src={thumbnail}
-              alt={`Thumbnail Image ${index + 1}`}
-            />
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
