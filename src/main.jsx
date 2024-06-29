25b2606ec2c8d95240efacf01d6b39d633a59b30
Eric <ericgeorge4477@gmail.com>
2024-06-30 02:22:12 +0300

    create context api

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import StoreContextProvider from "./context/StoreContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StoreContextProvider>
+    <App />
+  </StoreContextProvider>
+);
